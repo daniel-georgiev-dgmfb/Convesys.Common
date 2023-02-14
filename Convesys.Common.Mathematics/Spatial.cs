86e14cfedbd0eede781711d9ce5af4dcfc91c106
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-02-14 11:42:30 +0200

    adding location project

--- Convesys.Common.Mathematics/Spatial.cs
+++ Convesys.Common.Mathematics/Spatial.cs
@@ -20,18 +20,5 @@
             });
             return Task.FromResult(System.Math.Sqrt(distance));
         }
-
-        public static Task<Tuple<double, double>> GetLocation(Tuple<long, long, double> readings1, Tuple<long, long, double> readings2, Tuple<long, long, double> readings3)
-        {
-            var A = 2 * readings2.Item1 - 2 * readings1.Item1;
-            var B = 2 * readings2.Item2 - 2 * readings1.Item2;
-            var C = readings1.Item3 * readings1.Item3 - readings2.Item3 * readings2.Item3 - readings1.Item1 * readings1.Item1 + readings2.Item1 * readings2.Item1 - readings1.Item2 * readings1.Item2 + readings2.Item2 * readings2.Item2;
-            var D = 2 * readings3.Item1 - 2 * readings2.Item1;
-            var E = 2 * readings3.Item2 - 2 * readings2.Item2;
-            var F = readings2.Item3 * readings2.Item3 - readings3.Item3 * readings3.Item3 - readings2.Item1 * readings2.Item1 + readings3.Item1 * readings3.Item1 - readings2.Item2 * readings2.Item2 + readings3.Item2 * readings3.Item2;
-            var x = (C * E - F * B) / (E * A - B * D);
-            var y = (C * D - A * F) / (B * D - A * E);
-            return Task.FromResult(Tuple.Create(x, y));
-        }
     }
 }
---
+++ Platform.Common.Location.Tests.L0/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Platform.Common.Location.Tests.L0
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
