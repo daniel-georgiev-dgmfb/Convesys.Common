c5a506d9f0c0f8fefe9e6582af1d91e8e7a6e1bb
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-06-08 15:13:45 +0300

    centered point derivative impl

---
+++ Convesys.Common.Mathematics/CenteredFivePoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twilight.Platform.Common.Mathematics
+{
+    public class CenteredFivePoint
+    {
+        public static Task<double> Run(Func<double, double> f, double x)
+        {
+            double h = 10e-6;
+            double h2 = h * 2;
+            return Task.FromResult((f(x - h2) - 8 * f(x - h) + 8 * f(x + h) - f(x + h2)) / (h2 * 6));
+        }
+    }
+}
